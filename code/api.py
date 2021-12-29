688013791116a07d020e6b557871086798cea433
Terence Lau <terencelau@Terences-MBP.lan>
2021-12-29 13:14:27 +0800

    update api

--- code/api.py
+++ code/api.py
@@ -6,17 +6,39 @@ The original code is from gdelt-doc-api:
 "https://github.com/alex9smith/gdelt-doc-api/blob/main/gdeltdoc/api_client.py"
 by default, the system only provide at max 250 results
 thus, we are trying to remove the boundary by spliting
-the date range into multiple chunks
+the date range into multiple chunks:
+
+with start_date and end_date in filters, we split one day into two 12 hours:
+
+      --------------    ------------
+      | start date |    | end date |
+      --------------    ------------
+                              |
+------------------    ------------------
+| tmp start date |    | tmp end date 1 | (tmp end date = start date + 12 hours)
+------------------    ------------------                
+                              | + 12 hours
+--------------------  ------------------
+| tmp start date   |  | tmp end date 2 |
+| = tmp end date 1 |  |                |
+--------------------  ------------------           
+...
 """
 
 from datetime import datetime, timedelta
 import pandas as pd
 import requests
 import json
+import re
 
 def get_delta(date_1, date_2):
     delta = date_2 - date_1
     return delta
+
+def text_regex(str_1, str_2, newstring, text):
+    reg = "%s(.*?)%s" % (str_1,str_2)
+    r = re.compile(reg,re.DOTALL)
+    return (r.sub(newstring, text))
 
 def load_json(json_message, max_recursion_depth: int = 100, recursion_depth: int = 0):
     try:
@@ -48,18 +70,22 @@ def query_search(query_string = None, max_recursion_depth: int = 100):
             return pd.DataFrame(load_json(response.text, max_recursion_depth = max_recursion_depth)["articles"])
 
 
-def article_search(query_filter = None, max_recursion_depth: int = 100):
+def article_search(query_filter = None, max_recursion_depth:int = 100):
     articles_list = []
     if query_filter == None:
         return ValueError("Filter must be provided")
     else:
-        new_end_date = datetime.strptime(query_filter.start_date, "%Y-%m-%d-%H-%M-%S") + timedelta(hours=12)
+        new_end_date = datetime.strptime(query_filter.start_date, "%Y-%m-%d-%H-%M-%S") + timedelta(hours = 12) # tmp_end_date
         tmp_query_string = query_filter.query_string
-        while new_end_date <= datetime.strptime(query_filter.end_date, "%Y-%m-%d-%H-%M-%S"):
-            tmp_query_string = tmp_query_string.replace(query_filter.end_date.replace("-", ""), datetime.strftime(new_end_date, "%Y-%m-%d-%H-%M-%S").replace("-", ""))
-            tmp_articles = query_search(tmp_query_string, max_recursion_depth=100)
+        # tmp_end_date <= real end date
+        while new_end_date <= datetime.strptime(query_filter.end_date, "%Y-%m-%d-%H-%M-%S"): 
+            # subsitute the query parameters (enddatetime)
+            tmp_end_date_string = "&enddatetime=" + datetime.strftime(new_end_date, "%Y-%m-%d-%H-%M-%S").replace("-", "") + "&maxrecords"
+            tmp_query_string = text_regex(str_1="&enddatetime=", str_2 = "&maxrecords", newstring = tmp_end_date_string, text = tmp_query_string)
+            tmp_articles = query_search(tmp_query_string, max_recursion_depth = max_recursion_depth)
             articles_list.append(tmp_articles)
-            tmp_query_string = tmp_query_string.replace(query_filter.start_date.replace("-", ""), datetime.strftime(new_end_date, "%Y-%m-%d-%H-%M-%S").replace("-", ""))
-            new_end_date = new_end_date + timedelta(hours=12)
+            # subsitute the query parameters (startdatetime)
+            tmp_start_date_string = new_start_date = "&startdatetime=" + datetime.strftime(new_end_date, "%Y-%m-%d-%H-%M-%S").replace("-", "") + "&enddatetime="
+            new_end_date = new_end_date + timedelta(hours = 12)
             
         return pd.concat(articles_list)
