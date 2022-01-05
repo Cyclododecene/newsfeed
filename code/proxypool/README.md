8d6a0149844831446c2ebf6ae643a658f9824d53
TerenceLiu98 <terenceliu1012@gmail.com>
2022-01-06 01:11:21 +0800

    validility test

--- code/proxypool/README.md
+++ code/proxypool/README.md
@@ -2,24 +2,13 @@
 
 ## Some Free Proxy Source
 
-1. [Open-Proxy](https://openproxy.space/list)
-2. [US-Proxy](https://www.us-proxy.org/)
-3. [Geonode](https://geonode.com/free-proxy-list/)
-
-## Components 
-
-### Collection
-
-`Collection` is used for getting `ip` and `port` from multiple source - `getproxy.py`
-
-### Storage
-
-`Storage` is used for storing the proxies' information - `db.py`
-
-### Tester
-
-`Tester` is used for validating the availability of the proxies - `test.py`
-
-### API
-
-`api.py`
+```python
+proxy_source = {
+    "proxydb":"http://proxydb.net/?&availability=90",
+    "geonode":"https://proxylist.geonode.com/api/proxy-list?",
+    "ip3366":"http://www.ip3366.net/free/?",
+    "jiangxianli":"https://ip.jiangxianli.com/api/proxy_ips?",
+    "nimadaili":"http://nimadaili.com/",
+    "daili66":"http://www.66ip.cn/"
+}
+```
--- code/proxypool/validate.py
+++ code/proxypool/validate.py
@@ -1,4 +1,8 @@
 import os 
+import json
+import time
+from tkinter.messagebox import NO
+from weakref import proxy
 import requests
 from fake_useragent import UserAgent
 
@@ -13,33 +17,168 @@ def generate_header():
     return header
 
 class validater(object):
-    
-    http_validate_url = 'http://httpbin.org/ip'
-    https_validate_url = 'https://httpbin.org/ip'
-    validate_url = 'https://api.myip.com/'
 
-    def __init__(self, proxies:dict=None, proxy_type:str="https"):
+    def __init__(self, proxies:str, proxy_type:str="https"):
+        """
+        params: proxies:dict, proxy_type:str
+        """
+        self.http_validate_url = "http://httpbin.org/get"
+        self.https_validate_url = "https://httpbin.org/ip"
+        self.validate_url = "https://api.myip.com/"
+
         self.proxies = proxies
         self.type = proxy_type
-        self.http_test_url = ""
+        
+        self.status = None
+        self.response_time = None
+        self.anonymity = None
+        
         if proxies == None or proxy_type == None:
             return ValueError("proxies and proxy_type cannot be empty")
 
+    def _validate_proxy_status(self, proxies:dict, proxy_type:str):
+        """
+        output: status, response_time, speed, anonymity
+        """
+        status = self.status
+        response_time = self.response_time
+        anonymity = self.anonymity
+
+        if proxy_type == "http":
+            try:
+                start = time.time()
+                response = requests.get(self.http_validate_url, headers = generate_header(), proxies = proxies, timeout = 10)
+                if response.ok:
+                    status = True
+                    response_time = round(time.time() - start,3)
+                    resp_content = json.loads(response.text)
+                    header, ip = resp_content["headers"], resp_content["origin"]
+                    proxy_connection = header.get("Proxy-Connection", None)
+                    if "," in ip:
+                        anonymity = "transparent"
+                    elif proxy_connection:
+                        anonymity = "anonymous"
+                    else:
+                        anonymity = "elite"
+                        
+                    return status, response_time, anonymity
+                else:
+                    status = False
+                    return status, response_time, anonymity
+            except Exception as e:
+                status = False
+                return status, response_time, anonymity
+
+        elif proxy_type == "https":
+            try:
+                start = time.time()
+                response = requests.get(self.https_validate_url, headers = generate_header(), proxies = proxies, timeout = 10)
+                if response.ok:
+                    status = True
+                    response_time = round(time.time() - start,3)
+                    resp_content = json.loads(response.text)
+                    header, ip = resp_content["headers"], resp_content["origin"]
+                    proxy_connection = header.get("Proxy-Connection", None)
+                    if "," in ip:
+                        anonymity = "transparent"
+                    elif proxy_connection:
+                        anonymity = "anonymous"
+                    else:
+                        anonymity = "elite"
+                        
+                    return status, response_time, anonymity
+                else:
+                    status = False
+                    return status, response_time, anonymity
+            except Exception as e:
+                status = False
+                return status, response_time, anonymity
+
+        elif proxy_type == "socks5":
+            try:
+                start = time.time()
+                response = requests.get(self.https_validate_url, headers = generate_header(), timeout = 10)
+                if response.ok:
+                    status = True
+                    response_time = round(time.time() - start,3)
+                    resp_content = json.loads(response.text)
+                    header, ip = resp_content["headers"], resp_content["origin"]
+                    proxy_connection = header.get("Proxy-Connection", None)
+                    if "," in ip:
+                        anonymity = "transparent"
+                    elif proxy_connection:
+                        anonymity = "anonymous"
+                    else:
+                        anonymity = "elite"
+                        
+                    return status, response_time, anonymity
+                else:
+                    status = False
+                    return status, response_time, anonymity
+            except Exception as e:
+                status = False
+                return status, response_time, anonymity
+                    
     def validate(self):
         proxies = self.proxies
         proxy_type = self.type
-        proxies_status = []
-        for i in range(0,len(proxies)):
-            if proxy_type == "http":
-                proxy_dict = "http://" + proxies[i]
-                os.environ["http_proxy"] = proxy_dict
-                response = requests.get(self.http_test_url, headers = generate_header(), timeout = 2)
-                proxies_status.append(response.status_code)
-                return proxies_status
-            else:
-                proxy_dict = "https://" + proxies[i]
-                os.environ["https_proxy"] = proxy_dict
-                response = requests.get(self.https_test_url, headers = generate_header(), timeout = 2)
-                proxies_status.append(response.status_code)
-                return proxies_status
+        if proxy_type != "socks5":
+            proxies_dict = {"http:":"http://{}".format(proxies), "https:":"https://{}".format(proxies)}
 
+            http_status, http_response_time, http_anonymity = self._validate_proxy_status(proxies = proxies_dict, proxy_type = "http")
+            https_status, https_response_time, https_anonymity = self._validate_proxy_status(proxies = proxies_dict, proxy_type = "https")
+
+            if http_status and http_status:
+                proxy_type = "http & https"
+                status = http_status
+                anonymity = https_anonymity
+                response_time = https_response_time
+            elif http_status:
+                proxy_type = "http"
+                status = http_status
+                anonymity = http_anonymity
+                response_time = http_response_time
+            elif https_status:
+                proxy_type = "https"
+                status = https_status
+                anonymity = https_anonymity
+                response_time = https_response_time
+            else:
+                proxy_type = proxy_type
+                status = False
+                response_time = -1
+                anonymity = -1
+            
+            tested_proxy = {"origin":self.proxies, "type":proxy_type, "status":status, 
+                            "response_time":response_time, "anonymity":anonymity}
+            return tested_proxy
+        
+        else:
+            proxies_dict = {"http:":"socks5://{}".format(proxies), "https:":"socks5://{}".format(proxies)}
+
+            http_status, http_response_time, http_anonymity = self._validate_proxy_status(proxies = proxies_dict, proxy_type = "http")
+            https_status, https_response_time, https_anonymity = self._validate_proxy_status(proxies = proxies_dict, proxy_type = "https")
+
+            if http_status and http_status:
+                proxy_type = "socks5"
+                status = http_status
+                anonymity = https_anonymity
+                response_time = https_response_time
+            else:
+                status = https_status
+                response_time = -1
+                anonymity = -1
+            
+            tested_proxy = {"origin":self.proxies, "type":proxy_type, "status":status, 
+                            "response_time":response_time, "anonymity":anonymity}
+            return tested_proxy
+
+
+
+if __name__ == "__main__":
+    http_proxy = "103.149.162.194:80"
+    test = validater(proxies = http_proxy, proxy_type="https")
+    test.validate()
+    socks5_proxy = "18.162.79.205:10001"
+    test = validater(proxies = socks5_proxy, proxy_type="socks5")
+    test.validate()
