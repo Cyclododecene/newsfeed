c8e34eeff2153189cb65c9250f5488c81f6174d9
TerenceLiu98 <terenceliu1012@gmail.com>
2022-01-07 02:01:46 +0800

    test

--- code/proxypool/db.py
+++ code/proxypool/db.py
@@ -1,11 +1,10 @@
-from multiprocessing.sharedctypes import Value
 import sqlite3
 
 con = sqlite3.connect("data/proxy.db")
 cur = con.cursor()
 
 # create database
-def create_db(cursor=cur):
+def create_db(cursor=cur, db_name="Proxy"):
     """
     create table for  proxy with:
     1. RowID PRIMARY KEY - AUTO INCREMENT
@@ -20,9 +19,9 @@ def create_db(cursor=cur):
     10. ISP
     """
     try:
-        cur.execute("select count(*) from Proxy")
+        cur.execute("select count(*) from {}".format(db_name))
     except Exception as e:
-        cur.execute("create table Proxy(ID INTEGER PRIMARY KEY AUTOINCREMENT,\
+        cur.execute("create table {}(ID INTEGER PRIMARY KEY AUTOINCREMENT,\
                                         IP TEXT NOT NULL, \
                                         ProxyType TEXT,\
                                         Status TEXT,\
@@ -31,7 +30,7 @@ def create_db(cursor=cur):
                                         Country TEXT,\
                                         ShortCode TEXT, \
                                         Google INT, \
-                                        ISP TEXT);")
+                                        ISP TEXT);".format(db_name))
 
 def pass_data(cursor = cur, data:dict = None):
     """
@@ -52,3 +51,25 @@ def update_data(cursor = cur, data:dict = None):
         return ValueError("No data to update")
     else:
 """
+
+def update_adapted_db(cursor = cur, source_db:str = "Proxy", target_db:str = "AdaptedProxy") -> int:
+    cur.execute("DROP TABLE IF EXISTS {};".format(target_db))
+    create_db(cursor = cur, db_name = target_db)
+    cur.execute("INSERT INTO {} SELECT * FROM {} WHERE Status = 1 GROUP BY IP".format(target_db, source_db))
+    con.commit()
+    return 1
+
+def update_db_data(cursor = cur, db_name:str = "Proxy", data:dict = None):
+    if data == None:
+        return ValueError("No data to update")
+    else:
+        cur.execute("UPDATE {} SET Status = {} WHERE ID = '{}'".format(db_name, data["status"], data["ID"]))
+        return 1
+
+def select_db_data(cursor = cur, db_name:str = "Proxy", condition:str = "ID = 1") -> list:
+    cur.execute("SELECT * FROM {} WHERE {}".format(db_name, condition))
+    return cur.fetchall()
+
+
+if __name__ == "__main__":
+    create_db(db_name = "Proxy")
--- code/proxypool/main.py
+++ code/proxypool/main.py
@@ -9,9 +9,9 @@ from time import sleep
 
 from pathos.multiprocessing import ProcessPool as Pool
 
-#sleep(randint(10,100))
 
 proxies_list = []
+
 # proxydb - US
 countries_list, proxy_type_list, anonymity = ["US", "CN", "HK"], ["http", "https", "socks5"], ["anonymous", "elite", "transparent"]
 for i in tqdm(range(0, len(countries_list))):
@@ -27,8 +27,17 @@ for i in tqdm(range(0, len(countries_list))):
         for k in range(0, len(anonymity)):
             proxies_list.append(proxies(source_name = "geonode", country=countries_list[i], proxy_type=proxy_type_list[j], anonymity=anonymity[k]).collect())
 
+proxies_list.append(proxies(source_name = "jiangxianli").collect())
+proxies_list.append(proxies(source_name = "nimadaili", proxy_type = "https").collect())
+proxies_list.append(proxies(source_name = "nimadaili", proxy_type = "http").collect())
 proxies_list.append(proxies(source_name = "ip3366").collect())
+proxies_list.append(proxies(source_name = "daili66").collect())
 proxies_list.append(proxies(source_name = "daili66", country = "HK").collect())
+proxies_list.append(proxies(source_name = "daili66", country = "TW").collect())
+proxies_list.append(proxies(source_name = "proxy-list").collect())
+proxies_list.append(proxies(source_name = "spys", proxy_type = "https").collect())
+proxies_list.append(proxies(source_name = "spys", proxy_type = "http").collect())
+proxies_list.append(proxies(source_name = "spys", proxy_type = "socks5").collect())
 
 combined_list = [item for sublist in proxies_list for item in sublist]
 
@@ -41,13 +50,18 @@ for i in tqdm(range(0, len(combined_list))):
     pass_data(data = tmp_result)
 
 """
+start = time.time()
 for i in tqdm(range(0, len(combined_list), 5)):
-    tmp_list = combined_list
+    tmp_list = combined_list[i: i + 5]
     count = len(tmp_list)
     p = Pool(count)
     t = validater()
-    start = time.time()
     result = p.amap(validater.validate, [t] * count, tmp_list)
-    result.get()
-    round(time.time() - start,3)
-    sleep(randint(1,10))
+    tmp_results = result.get()
+    for i in range(0, len(tmp_results)):
+        pass_data(data = tmp_results[i])
+        
+round(time.time() - start,3)
+
+update_adapted_db()
+
